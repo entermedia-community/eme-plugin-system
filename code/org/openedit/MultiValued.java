09dcdc8d99d5c7de3969324cefe396a8be912642
Shakil Ahmmed <36266736+sXakil@users.noreply.github.com>
2026-08-01 05:42:27 +0600

    Refactor MultiValued and BaseData interfaces for improved readability and consistency; added JSON handling methods.

--- code/org/openedit/MultiValued.java
+++ code/org/openedit/MultiValued.java
@@ -6,32 +6,42 @@ import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
 import java.util.regex.Pattern;
-
+import org.json.simple.JSONObject;
 import org.openedit.modules.translations.LanguageMap;
 
 public interface MultiValued extends Data
 {
-	
+
 	public static final Pattern VALUEDELMITER = Pattern.compile("\\s*\\|\\s*");
-	
+
 	public Collection<String> getValues(String inPreference);
-	//public void setValues(String inKey, Collection<String> inValues);
-	public void addValue(String inKey, Object inNewValue); //adds to an existing collection
-	public void removeValue(String inKey, Object inNewValue); 
-	public boolean containsValue(String inKey, Object inNewValue); 
-	
+
+	// public void setValues(String inKey, Collection<String> inValues);
+	public void addValue(String inKey, Object inNewValue); // adds to an existing collection
+
+	public void removeValue(String inKey, Object inNewValue);
+
+	public boolean containsValue(String inKey, Object inNewValue);
+
 	public Date getDate(String inField);
+
 	public String getText(String inId, String inLocale);
+
 	public boolean getBoolean(String inId);
+
 	public Double getDouble(String inId);
+
 	public int getInt(String inId);
+
 	public float getFloat(String inId);
+
 	public long getLong(String inId);
+
 	public void addValues(String inString, Collection inValues);
-	
-	public static List<Double> collectDoubles(String[] vector) 
+
+	public static List<Double> collectDoubles(String[] vector)
 	{
-		List<Double> doubles = new ArrayList(vector.length );
+		List<Double> doubles = new ArrayList(vector.length);
 		for (int i = 0; i < vector.length; i++)
 		{
 			double f = Double.parseDouble(vector[i]);
@@ -39,16 +49,17 @@ public interface MultiValued extends Data
 		}
 		return doubles;
 	}
-	public static List<Double> collectDoubles(Collection vector) 
+
+	public static List<Double> collectDoubles(Collection vector)
 	{
 		List<Double> doubles = new ArrayList(vector.size());
 		for (Iterator iterator = vector.iterator(); iterator.hasNext();)
 		{
 			Object floatobj = iterator.next();
 			double f;
-			if( floatobj instanceof Double)
+			if (floatobj instanceof Double)
 			{
-				f = (Double)floatobj;
+				f = (Double) floatobj;
 			}
 			else
 			{
@@ -58,9 +69,10 @@ public interface MultiValued extends Data
 		}
 		return doubles;
 	}
-	public static List<Float> collectFloats(String[] vector) 
+
+	public static List<Float> collectFloats(String[] vector)
 	{
-		List<Float> floats = new ArrayList(vector.length );
+		List<Float> floats = new ArrayList(vector.length);
 		for (int i = 0; i < vector.length; i++)
 		{
 			float f = Float.parseFloat(vector[i]);
@@ -68,15 +80,17 @@ public interface MultiValued extends Data
 		}
 		return floats;
 	}
-	public static List<Float> collectFloats(Collection vector) {
+
+	public static List<Float> collectFloats(Collection vector)
+	{
 		List<Float> floats = new ArrayList(vector.size());
 		for (Iterator iterator = vector.iterator(); iterator.hasNext();)
 		{
 			Object floatobj = iterator.next();
 			float f;
-			if( floatobj instanceof Float || floatobj instanceof Double)
+			if (floatobj instanceof Float || floatobj instanceof Double)
 			{
-				f = (float)floatobj;
+				f = (float) floatobj;
 			}
 			else
 			{
@@ -86,8 +100,16 @@ public interface MultiValued extends Data
 		}
 		return floats;
 	}
-	
+
 	public LanguageMap getLanguageMap(String inId);
 
 	public boolean hasValue(String inKey);
+
+	public JSONObject getJSONValue(String inKey);
+
+	public Object getJSONValue(String inFieldKey, String inSubKey);
+
+	public void setJSONValue(String inKey, JSONObject inValue);
+
+	public void setJSONValue(String inFieldKey, String inSubKey, Object inValue);
 }
--- code/org/openedit/data/BaseData.java
+++ code/org/openedit/data/BaseData.java
@@ -5,11 +5,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
-
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
@@ -20,26 +18,23 @@ import org.openedit.WebPageRequest;
 import org.openedit.modules.translations.LanguageMap;
 import org.openedit.util.DateStorageUtil;
 
-
 public class BaseData implements MultiValued, Comparable, Cloneable
 {
-	public static final Data NULL = new BaseData(); 
-	
+	public static final Data NULL = new BaseData();
+
 	protected ValuesMap fieldProperties;
 
-	public BaseData() {
-	}
+	protected JSONParser fieldJsonParser = new JSONParser();
 
+	public BaseData() {}
 
-	
-	public BaseData(Map inMap) 
-	{
+	public BaseData(Map inMap) {
 		fieldProperties = new ValuesMap(inMap);
 	}
 
-	public String get(String inId) 
+	public String get(String inId)
 	{
-		if( fieldProperties == null || inId == null)
+		if (fieldProperties == null || inId == null)
 		{
 			return null;
 		}
@@ -47,21 +42,22 @@ public class BaseData implements MultiValued, Comparable, Cloneable
 		String stringvalue = getProperties().toString(value);
 		return stringvalue;
 	}
-	
-	public String getText(String inId, WebPageRequest inContext) {
-		if(Boolean.parseBoolean(inContext.getPageProperty("auto_translate"))){
-			String locale =  inContext.getLocale();
+
+	public String getText(String inId, WebPageRequest inContext)
+	{
+		if (Boolean.parseBoolean(inContext.getPageProperty("auto_translate")))
+		{
+			String locale = inContext.getLocale();
 
 			return getText(inId, locale);
 		}
-		
+
 		return getText(inId, "en");
 	}
-	
-	
-	public String getText(String inId, String inLocale) 
+
+	public String getText(String inId, String inLocale)
 	{
-		if( fieldProperties == null)
+		if (fieldProperties == null)
 		{
 			return null;
 		}
@@ -70,7 +66,7 @@ public class BaseData implements MultiValued, Comparable, Cloneable
 		{
 			return null;
 		}
-		
+
 		if (value instanceof LanguageMap)
 		{
 			LanguageMap map = getLanguageMap(inId, value);
@@ -78,7 +74,7 @@ public class BaseData implements MultiValued, Comparable, Cloneable
 			{
 				return null;
 			}
-			
+
 			return map.getText(inLocale);
 		}
 		if (value instanceof Map)
@@ -87,10 +83,10 @@ public class BaseData implements MultiValued, Comparable, Cloneable
 		}
 		return value.toString();
 	}
-	
-	public LanguageMap getLanguageMap(String inId) 
+
+	public LanguageMap getLanguageMap(String inId)
 	{
-		if( fieldProperties == null)
+		if (fieldProperties == null)
 		{
 			return null;
 		}
@@ -102,138 +98,129 @@ public class BaseData implements MultiValued, Comparable, Cloneable
 		return getLanguageMap(inId, value);
 	}
 
-
-
 	protected LanguageMap getLanguageMap(String inId, Object value)
 	{
-		if( value instanceof LanguageMap )
+		if (value instanceof LanguageMap)
 		{
-			LanguageMap map = (LanguageMap)value;
+			LanguageMap map = (LanguageMap) value;
 			return map;
 		}
-		else if( value instanceof Map)
+		else if (value instanceof Map)
 		{
-			LanguageMap map = new LanguageMap((Map)value);
+			LanguageMap map = new LanguageMap((Map) value);
 			return map;
 		}
 		LanguageMap map = new LanguageMap();
 		map.setText("en", (String) value);
 		return map;
 	}
-	
+
 	public boolean getBoolean(String inId)
 	{
 		Object val = getValue(inId);
-		if( val == null  )
+		if (val == null)
 		{
 			return false;
 		}
-		if( val instanceof Boolean)
+		if (val instanceof Boolean)
 		{
-			return (boolean)val;
+			return (boolean) val;
 		}
 		return Boolean.valueOf(val.toString());
 	}
-	
+
 	public float getFloat(String inId)
 	{
 		Object val = getValue(inId);
-		if( val == null )
+		if (val == null)
 		{
 			return 0;
 		}
-		if( val instanceof Float)
+		if (val instanceof Float)
 		{
-			return (float)val;
+			return (float) val;
 		}
 		return Float.parseFloat(val.toString());
 	}
-	
-	
+
 	public Double getDouble(String inId)
 	{
 		Object val = getValue(inId);
-		if( val == null )
+		if (val == null)
 		{
 			return null;
 		}
-		if( val instanceof Double)
+		if (val instanceof Double)
 		{
-			return (double)val;
+			return (double) val;
 		}
 		return Double.parseDouble(val.toString());
 	}
-	
+
 	public long getLong(String inId)
 	{
 		Object val = getValue(inId);
-		if( val == null )
+		if (val == null)
 		{
 			return 0;
 		}
-		if( val instanceof Long)
+		if (val instanceof Long)
 		{
-			return (long)val;
+			return (long) val;
 		}
 		return Long.parseLong(val.toString());
 	}
-	
-	
-	
-	
+
 	public int getInt(String inId)
 	{
 		Object val = getValue(inId);
-		if( val == null )
+		if (val == null)
 		{
 			return 0;
 		}
-		if( val instanceof Integer)
+		if (val instanceof Integer)
 		{
-			return (int)val;
+			return (int) val;
 		}
 		return Integer.parseInt(val.toString().trim());
 	}
-	
-	
-	public String getId() {
+
+	public String getId()
+	{
 		String name = get("id");
 		return name;
 	}
-	
-	
-	
-	
-	public String getName(String locale) 
+
+	public String getName(String locale)
 	{
 		Object name = getValue("name");
-		if( name instanceof LanguageMap)
+		if (name instanceof LanguageMap)
 		{
-			LanguageMap values = (LanguageMap)name;
+			LanguageMap values = (LanguageMap) name;
 			String val = values.getDefaultText(locale);
 			return val;
 		}
-		return (String)name;
+		return (String) name;
 	}
-	
-	public String getName(WebPageRequest inReq) {
-		if(Boolean.parseBoolean(inReq.getPageProperty("auto_translate"))){
-			String locale =  inReq.getLocale();
 
-			return getName( locale);
+	public String getName(WebPageRequest inReq)
+	{
+		if (Boolean.parseBoolean(inReq.getPageProperty("auto_translate")))
+		{
+			String locale = inReq.getLocale();
+
+			return getName(locale);
 		}
 		return getName("en");
 	}
-	
-	
-	
 
-	public String getName() 
+	public String getName()
 	{
 		return get("name");
 	}
 
-	public void setName(String inName) {
+	public void setName(String inName)
+	{
 		Object intname = getValue("name");
 		if (intname instanceof LanguageMap)
 		{
@@ -245,34 +232,39 @@ public class BaseData implements MultiValued, Comparable, Cloneable
 		}
 	}
 
-	public String toString() {
+	public String toString()
+	{
 		String name = getName();
-		if (name == null) {
+		if (name == null)
+		{
 			return super.toString();
 		}
 		return name;
 	}
 
-	public void setProperty(String inId, String inValue) 
+	public void setProperty(String inId, String inValue)
 	{
-		if (inValue == null) 
+		if (inValue == null)
 		{
 			getProperties().remove(inId);
-		} else {
+		}
+		else
+		{
 			setValue(inId, inValue);
 		}
 	}
+
 	/**
 	 * @deprecated This API should be String based for now
 	 * @param inId
 	 * @param inValue
 	 */
-	public void setPropertyValues(String inId, String[] inValues) 
+	public void setPropertyValues(String inId, String[] inValues)
 	{
 		StringBuffer buffer = new StringBuffer();
 		for (int i = 0; i < inValues.length; i++)
 		{
-			if( i > 0)
+			if (i > 0)
 			{
 				buffer.append(" | ");
 			}
@@ -293,159 +285,168 @@ public class BaseData implements MultiValued, Comparable, Cloneable
 		}
 	}
 
-	public void setId(int inNewid) {
-		setProperty("id", String.valueOf( inNewid) );
+	public void setId(int inNewid)
+	{
+		setProperty("id", String.valueOf(inNewid));
 	}
 
-	public void setId(String inNewid) 
+	public void setId(String inNewid)
 	{
-		if( inNewid == null)
+		if (inNewid == null)
 		{
 			inNewid = ValuesMap.NULLSTRING;
 		}
 		setProperty("id", inNewid);
 	}
 
-	public String getSourcePath() {
+	public String getSourcePath()
+	{
 		return get("sourcepath");
 	}
 
-	public void setSourcePath(String inSourcepath) {
+	public void setSourcePath(String inSourcepath)
+	{
 		setProperty("sourcepath", inSourcepath);
 	}
-	
-	//DO not call
-	public ValuesMap getProperties() 
+
+	// DO not call
+	public ValuesMap getProperties()
 	{
-		if (fieldProperties == null) {
+		if (fieldProperties == null)
+		{
 			fieldProperties = new ValuesMap();
 		}
 		return fieldProperties;
 
 	}
-	//only Adds properties does not remove any
+
+	// only Adds properties does not remove any
 	public void setProperties(Map inProperties)
 	{
 		getProperties().putAll(inProperties);
 	}
-	public int compareTo(Object inO) {
+
+	public int compareTo(Object inO)
+	{
 		BaseData inData = (BaseData) inO;
-		if (getName() != null && inData.getName() != null) {
+		if (getName() != null && inData.getName() != null)
+		{
 			return getName().toLowerCase().compareTo(inData.getName().toLowerCase());
 		}
 		return 0;
 	}
-	
+
 	public Collection<String> getValues(String inPreference)
 	{
 		return getProperties().getValues(inPreference);
 	}
+
 	public boolean hasValue(String inField, String inId)
 	{
-		if( getProperties().containsKey(inField) )
+		if (getProperties().containsKey(inField))
 		{
-			return getProperties().containsInValues(inField,inId);
+			return getProperties().containsInValues(inField, inId);
 		}
 		Object object = getValue(inField);
-		if( object == null)
+		if (object == null)
 		{
 			return false;
 		}
-		if( object instanceof Collection)
+		if (object instanceof Collection)
 		{
-			boolean had =  ((Collection<String>)object).contains(inId);
+			boolean had = ((Collection<String>) object).contains(inId);
 			return had;
 		}
-		if( object instanceof String[])
+		if (object instanceof String[])
 		{
-			Collection<String> values = Arrays.asList((String[])object);
-			boolean had =  values.contains(inId);
+			Collection<String> values = Arrays.asList((String[]) object);
+			boolean had = values.contains(inId);
 			return had;
 		}
-		if( inId.equals(object) )
+		if (inId.equals(object))
 		{
 			return true;
 		}
 		return false;
 	}
-	
+
 	public Date getDate(String inField)
 	{
 		Object date = getValue(inField);
-		if(date == null)
+		if (date == null)
 		{
 			return null;
 		}
 
-		if(date instanceof Date){
+		if (date instanceof Date)
+		{
 			return (Date) date;
 		}
-		
-		return DateStorageUtil.getStorageUtil().parseFromStorage((String)date);
-		
-		
+
+		return DateStorageUtil.getStorageUtil().parseFromStorage((String) date);
+
 	}
-	
+
 	public Object getValue(String inKey)
 	{
 		Object val = getProperties().getValue(inKey);
-		if( val == ValuesMap.NULLSTRING)
+		if (val == ValuesMap.NULLSTRING)
 		{
 			val = null;
 		}
 		/*
-		if( val == null && inKey.equals("name"))
-		{
-			Map map = (Map)getProperties().getValue(inKey  + "_int"); //Not needed, remove this
-			return map;
-		}*/
+		 * if( val == null && inKey.equals("name")) { Map map = (Map)getProperties().getValue(inKey +
+		 * "_int"); //Not needed, remove this return map; }
+		 */
 		return val;
 	}
+
 	public void setValue(String inKey, Object inValue)
 	{
-		if( inKey.equals("emrecordstatus") && inValue instanceof String)
+		if (inKey.equals("emrecordstatus") && inValue instanceof String)
 		{
-			//Spreadsheet importing?
-			String val = (String)inValue;
-			if( val.trim().isEmpty())
+			// Spreadsheet importing?
+			String val = (String) inValue;
+			if (val.trim().isEmpty())
 			{
 				return;
 			}
 			try
 			{
 				JSONParser parser = new JSONParser();
-				inValue = (Map)parser.parse(val);
+				inValue = (Map) parser.parse(val);
 			}
 			catch (ParseException e)
 			{
 				throw new OpenEditException(e);
 			}
 		}
-		getProperties().put(inKey,inValue);
+		getProperties().put(inKey, inValue);
 	}
-	
+
 	/**
 	 * @deprecated use setValue
 	 */
 	public void setValues(String inKey, Collection<String> inValues)
 	{
-//		StringBuffer values = new StringBuffer();
-//		for (Iterator iterator = inValues.iterator(); iterator.hasNext();)
-//		{
-//			String detail = (String) iterator.next();
-//			values.append(detail);
-//			if( iterator.hasNext())
-//			{
-//				values.append(" | ");
-//			}
-//		}
+		// StringBuffer values = new StringBuffer();
+		// for (Iterator iterator = inValues.iterator(); iterator.hasNext();)
+		// {
+		// String detail = (String) iterator.next();
+		// values.append(detail);
+		// if( iterator.hasNext())
+		// {
+		// values.append(" | ");
+		// }
+		// }
 		getProperties().put(inKey, inValues);
 	}
+
 	public void addValue(String inKey, Object inNewValue)
 	{
-		if( inNewValue instanceof Collection)
+		if (inNewValue instanceof Collection)
 		{
-			for (Iterator iterator = ((Collection)inNewValue).iterator(); iterator.hasNext();)
+			for (Iterator iterator = ((Collection) inNewValue).iterator(); iterator.hasNext();)
 			{
 				Object value = (Object) iterator.next();
 				addValue(inKey, value);
@@ -453,20 +454,20 @@ public class BaseData implements MultiValued, Comparable, Cloneable
 			return;
 		}
 		Collection values = getValues(inKey);
-		if(values == null) 
+		if (values == null)
 		{
 			values = new ArrayList();
 		}
-		
-		if(!values.contains(inNewValue)) 
+
+		if (!values.contains(inNewValue))
 		{
 			values = new ArrayList(values);
-			values.add(inNewValue);			
+			values.add(inNewValue);
 			setValue(inKey, values);
 		}
-			
+
 	}
-	
+
 	public void addValues(String inKey, Collection inNewValues)
 	{
 		if (inNewValues == null)
@@ -476,14 +477,14 @@ public class BaseData implements MultiValued, Comparable, Cloneable
 		for (Iterator iterator = inNewValues.iterator(); iterator.hasNext();)
 		{
 			Object value = (Object) iterator.next();
-				addValue(inKey, value);
+			addValue(inKey, value);
 		}
 	}
-	
+
 	public void removeValue(String inKey)
 	{
 		getProperties().remove(inKey);
-		
+
 	}
 
 	@Override
@@ -492,50 +493,49 @@ public class BaseData implements MultiValued, Comparable, Cloneable
 		getProperties().removeValue(inKey, inOldValue);
 	}
 
-	
 	@Override
 	public Set keySet()
 	{
 		return getProperties().keySet();
 	}
-	
+
 	protected Long toLong(Number inNumber)
 	{
-		if( inNumber instanceof Long)
+		if (inNumber instanceof Long)
 		{
-			return (Long)inNumber;
+			return (Long) inNumber;
 		}
-		if( inNumber instanceof Integer)
+		if (inNumber instanceof Integer)
 		{
-			return ((Integer)inNumber).longValue();
+			return ((Integer) inNumber).longValue();
 		}
 		throw new OpenEditException("Number is not a valid Long");
-		
+
 	}
-	
+
 	public String getAge()
 	{
 		return getAge("creationdate");
 	}
-	
+
 	public String getAge(String InDateField)
 	{
 		Date createdon = getDate(InDateField);
-		if( createdon == null)
+		if (createdon == null)
 		{
 			return null;
 		}
-		//MathUtils util = new MathUtils();
+		// MathUtils util = new MathUtils();
 		long diff = System.currentTimeMillis() - createdon.getTime();
-		
+
 		long minute = (diff / (1000 * 60)) % 60;
 		long hour = (diff / (1000 * 60 * 60));
 		String time = null;
-		if( hour > 24)
+		if (hour > 24)
 		{
-			double days = (double)hour / 24d;
+			double days = (double) hour / 24d;
 			hour = hour % 24;
-			time = String.format("%2dd:%2dh:%2dm", (int)days, hour, minute);
+			time = String.format("%2dd:%2dh:%2dm", (int) days, hour, minute);
 		}
 		else
 		{
@@ -543,79 +543,131 @@ public class BaseData implements MultiValued, Comparable, Cloneable
 		}
 		return time;
 	}
-	
 
 	public boolean containsValue(String inKey, Object inValue)
 	{
 		Collection current = getValues(inKey);
-		
-		if( current != null && current.contains(inValue))
+
+		if (current != null && current.contains(inValue))
 		{
 			return true;
 		}
 		return false;
 	}
 
-	
 	public Data copy()
 	{
-		Data copy = (Data)clone();
+		Data copy = (Data) clone();
 		return copy;
 	}
-	
-	
-	
+
 	public String toJsonString()
 	{
-		/*StringBuffer output = new StringBuffer();
-		output.append("{ \"_id\": \"" + getId() + "\",");
-		output.append(" \"map\" :");
-		JSONObject object = new JSONObject(getProperties());  //TODO: Deal with Java Objects. Loop over stuff?
-		output.append(object.toJSONString());
-		output.append(" \n}");
-		return output.toString();*/
-		
-		//JSONObject output = new JSONObject();
-		//output.put("_id", getId());
+		/*
+		 * StringBuffer output = new StringBuffer(); output.append("{ \"_id\": \"" + getId() + "\",");
+		 * output.append(" \"map\" :"); JSONObject object = new JSONObject(getProperties()); //TODO: Deal
+		 * with Java Objects. Loop over stuff? output.append(object.toJSONString()); output.append(" \n}");
+		 * return output.toString();
+		 */
+
+		// JSONObject output = new JSONObject();
+		// output.put("_id", getId());
 		Map properties = getProperties();
-		
+
 		for (Iterator iterator = properties.keySet().iterator(); iterator.hasNext();)
 		{
 			String key = (String) iterator.next();
 			Object value = properties.get(key);
-			
+
 			if (value instanceof Date)
 			{
-				String jsondate = DateStorageUtil.getStorageUtil().formatDateObj((Date)value, "yyyy-MM-dd'T'HH:mm:ss.SSSZ");
+				String jsondate = DateStorageUtil.getStorageUtil().formatDateObj((Date) value, "yyyy-MM-dd'T'HH:mm:ss.SSSZ");
 				properties.put(key, jsondate);
 			}
-			
-		}
-				
-		JSONObject object = new JSONObject(properties);  //TODO: Deal with Java Objects. Loop over stuff?
-		//output.put("map", object);
 
-		//return output.toJSONString();
+		}
+
+		JSONObject object = new JSONObject(properties); // TODO: Deal with Java Objects. Loop over stuff?
+		// output.put("map", object);
+
+		// return output.toJSONString();
 		return object.toJSONString();
-	}	
-	
+	}
+
 	public boolean hasValue(String inKey)
 	{
 		Object value = getValue(inKey);
 		if (value != null)
 		{
-			if( value instanceof String)
+			if (value instanceof String)
 			{
-				boolean empty = ((String)value).trim().isEmpty();
+				boolean empty = ((String) value).trim().isEmpty();
 				return !empty;
 			}
 			return true;
 		}
 		return false;
-	}	
-	public Instant getInstant(String key) {
-	    Date d = getDate(key); // existing
-	    return d != null ? d.toInstant() : null;
 	}
-	
+
+	public Instant getInstant(String key)
+	{
+		Date d = getDate(key); // existing
+		return d != null ? d.toInstant() : null;
+	}
+
+	public JSONObject getJSONValue(String inKey)
+	{
+		String value = get(inKey);
+		if (value == null)
+		{
+			return null;
+		}
+		try
+		{
+			return (JSONObject) fieldJsonParser.parse(value);
+		}
+		catch (ParseException e)
+		{
+			return null;
+		}
+	}
+
+	public Object getJSONValue(String inFieldKey, String inSubKey)
+	{
+		JSONObject json = getJSONValue(inFieldKey);
+		if (json == null)
+		{
+			return null;
+		}
+		return json.get(inSubKey);
+	}
+
+	public void setJSONValue(String inKey, JSONObject inValue)
+	{
+		if (inValue == null)
+		{
+			setProperty(inKey, null);
+		}
+		else
+		{
+			setProperty(inKey, inValue.toJSONString());
+		}
+	}
+
+	public void setJSONValue(String inFieldKey, String inSubKey, Object inValue)
+	{
+		if (!(inValue instanceof String) && !(inValue instanceof Number) && !(inValue instanceof Boolean) && !(inValue instanceof JSONObject) && inValue != null)
+		{
+			throw new OpenEditException("Only String, Number, Boolean, JSONObject or null are allowed for JSON values");
+		}
+		JSONObject json = getJSONValue(inFieldKey);
+		if (json == null)
+		{
+			json = new JSONObject();
+		}
+		json.put(inSubKey, inValue);
+
+		setJSONValue(inFieldKey, json);
+	}
+
 }
